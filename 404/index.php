c9b9f611bdaea5279bb06a24cf1608d847c374a5
Miguel Serrano <maserranocs@outlook.com>
2020-09-13 10:19:31 -0600

    initial commit

---
+++ 404/index.php
@@ -0,0 +1,77 @@
+<?php
+
+
+echo '
+
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page Not Found</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="icon" href="img/icons/favicon.png">
+    <!-- All CSS Files -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/themify-icons.css">
+    <link rel="stylesheet" href="../css/elements.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <!-- User style -->
+    <link rel="stylesheet" href="../css/custom.css">
+    <script src="../js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- Start main wrapper -->
+    <div class="wrapper">
+        <!-- Header area start -->
+        
+        <div class="intelligent-header-space"></div>
+        <!-- Header area End -->
+        <!-- Main content start -->
+        <div class="content">
+            <!-- Breadcrumb area start -->
+            
+            <!-- Breadcrumb area end -->
+            <!-- Not found content start -->
+            <div class="row">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="not-found-content text-center">
+                                <h1>404</h1>
+                                <h3>Ooopps.! the page you were looking for, couldn\'t be found.</h3>
+                                <a href="../" class="button medium">Home</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Not found content end -->
+        </div>
+        <!-- Main content end -->
+        <!-- Footer area start -->
+        
+        <!-- Footer area end -->
+        <!-- Footer area end -->
+    </div>
+    <!-- End main wrapper -->
+    <!-- All JS Files -->
+    <script src="js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/jquery.isotope.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
+';
+?>
